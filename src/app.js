86b56141e586a859e7fed2d1d0e947627d7b1649
gusscamargo <guga.power@hotmail.com>
2021-12-16 02:56:18 -0300

    Adicionando o esqueleto base do projeto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
-var http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = 3000;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Ola Mundo');
-}).listen(port);
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Server config
+const port = process.env.NODEJS | 3000
+
+app.listen(port, () => console.log("Server ON"))
